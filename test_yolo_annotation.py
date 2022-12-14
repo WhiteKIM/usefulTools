6a604a561eec5cfb343bf54b4d57a0fbbd1985fd
WhiteKIM <9rudrb40@naver.com>
2022-12-14 09:09:17 +0900

    2022.12.14 upload new code file

---
+++ test_yolo_annotation.py
@@ -0,0 +1,43 @@
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFont, ImageDraw
+import numpy as np
+import os
+
+def plot_bounding_box(image, annotation_list):
+    annotations = np.array(annotation_list)
+    w, h = image.size
+    
+    plotted_image = ImageDraw.Draw(image)
+
+    transformed_annotations = np.copy(annotations)
+    transformed_annotations[:,[1,3]] = annotations[:,[1,3]] * w
+    transformed_annotations[:,[2,4]] = annotations[:,[2,4]] * h 
+    
+    transformed_annotations[:,1] = transformed_annotations[:,1] - (transformed_annotations[:,3] / 2)
+    transformed_annotations[:,2] = transformed_annotations[:,2] - (transformed_annotations[:,4] / 2)
+    transformed_annotations[:,3] = transformed_annotations[:,1] + transformed_annotations[:,3]
+    transformed_annotations[:,4] = transformed_annotations[:,2] + transformed_annotations[:,4]
+    
+    for ann in transformed_annotations:
+        obj_cls, x0, y0, x1, y1 = ann
+        plotted_image.rectangle(((x0,y0), (x1,y1)))
+        
+    
+    plt.imshow(np.array(image))
+    plt.show()
+
+with open('a10_100.txt', "r") as file:
+    annotation_list = file.read().split("\n")[:-1]
+    annotation_list = [x.split(" ") for x in annotation_list]
+    annotation_list = [[float(y) for y in x ] for x in annotation_list]
+
+#Get the corresponding image file
+image_file = 'a10_100.jpg'
+assert os.path.exists(image_file)
+
+#Load the image
+image = Image.open(image_file)
+
+#Plot the Bounding Box
+plot_bounding_box(image, annotation_list)
